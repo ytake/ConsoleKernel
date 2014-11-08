7b06f0f3f8c5f15cb9af8de85edf84303c7f8c8e
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-11-08 20:20:15 +0900

    added test code

--- phpunit.php
+++ phpunit.php
@@ -1,5 +1,6 @@
 <?php
-
 require __DIR__.'/vendor/autoload.php';
 
 date_default_timezone_set('Asia/Tokyo');
+
+define("TEST_DIR", __DIR__);
--- tests/TokenizerTest.php
+++ tests/TokenizerTest.php
@@ -14,6 +14,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
     {
         parent::setUp();
         $container = new \Iono\Console\Container();
+        $this->setConfigure($container);
         $this->tokenizer = new \Iono\Console\Tokenizer(
             $container, new \Colors\Color()
         );
@@ -30,6 +31,23 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(1, $this->tokenizer->getProperty("TestClass", "publicProperty"));
         $this->assertSame(1, $this->tokenizer->getProperty("TestClass", "protectedProperty"));
         $this->assertSame(1, $this->tokenizer->getProperty("TestClass", "privateProperty"));
+    }
+
+    public function testApplication()
+    {
+        $this->assertInstanceOf("Iono\Console\Container", $this->tokenizer->getApplication());
+    }
+
+    /**
+     * @param \Iono\Console\Container $container
+     */
+    protected function setConfigure(\Iono\Console\Container $container)
+    {
+        $container['directory'] = require TEST_DIR ."/tests/path.php";
+        $container->bindShared('component.config', function () use($container) {
+                return \Iono\Console\Application\Configure::registerConfigure($container);
+            }
+        );
     }
 }
 
